94842652fd9efa508df5d93df1308de56b26c17b
Paulo-Sergio-Oliveira <99352391+Paulo-Sergio-Oliveira@users.noreply.github.com>
2022-08-28 22:31:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    //Variáveis
+    $nome = "Paulo Sergio";
+    $idade = 16;
+    $veiculo = "carro";
+
+    //Variáveis de variáveis
+    $carro = "Lamborghini";
+
+    //Concatenar
+    echo "O meu nome é ".$nome.". <br> Tenho ".$idade." anos. <br>"."Sempre dou uma volta no meu ".$veiculo.
+    " que é uma ".$$veiculo.".";
+
+    //Comparar valores
+    //==(igual) !=(diferente) >=(maior igual) <=(menor igual) >(maior) <(menor) ===(totalmente igual) !==(totalmente diferente)
+    if($idade == 16){
+        echo "<br> Sua idade está correta!";
+    }else{
+        echo "<br> Sua idade está incorreta!";
+    }
+
+    //Looping
+    for($i = 10; $i >= 0; $i--){
+        if($i == 0){
+            echo "<br>".$i."<br> Acabou!";
+        }else{
+            echo "<br>".$i;
+        }
+    }
+
+    $i = 0;
+    while($i <= 10){
+        if($i == 10){
+            echo "<br>".$i."<br> Acabou!";
+        }else{
+            echo "<br>".$i;
+        }
+    $i++;
+    }
+
+    //Funções
+    function printNome($n){
+        echo "<br> O nome dele é ".$n.".";
+    }
+    printNome("Thierry");
+
+    //Objetos
+    class Pessoa{
+        public $nome;
+        public $idade;
+
+        public function __construct($nome,$idade){
+            $this->nome = $nome;
+            $this->idade = $idade;
+        }
+
+        public function printNomeEIdade(){
+            echo "<br> O nome dele é ".$this->nome.". <br>";
+            echo "Ele tem ".$this->idade." anos.";
+        }
+    }
+    $pessoa = new Pessoa("Vinicio","16");
+    $pessoa->printNomeEIdade();
+?>
